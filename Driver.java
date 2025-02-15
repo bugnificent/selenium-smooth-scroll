5a5ccd256936fd77a1cc79cdd45f1f02c2ed5c78
Yusuf <yusuf.asik123@gmail.com>
2025-02-15 13:10:02 +0300

    Updated

--- Driver.java
+++ Driver.java
@@ -1,7 +1,9 @@
 package com.velespit.tests.utilities;
 
+import com.velespit.utilities.ConfigurationReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -10,69 +12,53 @@ import java.time.Duration;
 
 public class Driver {
 
-    //create a private constructor to remove access to this object
+
     private Driver() {
     }
+    
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
-    /*
-    We make the WebDriver private, because we want to close access from outside the class.
-    We are making it static, because we will use it in a static method.
-     */
-    private static WebDriver driver; // default value = null
+    public static WebDriver getDriver(){
 
-    /*
-    Create a re-usable utility method which will return the same driver instance once we call it.
-    - If an instance doesn't exist, it will create first, and then it will always return same instance.
-     */
-    public static WebDriver getDriver() {
-        if (driver == null) {
-            /*
-            We will read our browserType from configuration.properties file.
-            This way, we can control which browser is opened from outside our code.
-             */
+        if(driverPool.get() == null){
+
             String browserType = ConfigurationReader.getProperty("browser");
-             /*
-            Depending on the browserType returned from the configuration.properties
-            switch statement will determine the "case", and open the matching browser.
-             */
-            switch (browserType) {
+
+            switch (browserType){
                 case "chrome":
                     //WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
                 case "firefox":
-                    driver = new FirefoxDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    //WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
-                case "edge":
-                    driver = new EdgeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                    break;
-                case "safari":
-                    driver = new SafariDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                case "headless-chrome":e
+                    // WebDriverManager.chromedriver().setup();
+                    ChromeOptions option = new ChromeOptions();
+                    option.addArguments("--headless=new");
+                    driverPool.set(new ChromeDriver(option));
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
             }
+
         }
 
-        return driver;
+        return driverPool.get();
+
     }
 
-    public static void closeDriver() {
-        if (driver != null) {
-            /*
-            This line will terminate the currently existing driver completely. It will not exist going forward.
-             */
-            driver.quit();
-            /*
-            We assign the value back to "null" so that my "singleton" can create a newer one if needed.
-             */
-            driver = null;
+    public static void closeDriver(){
+        if (driverPool.get()!=null){
+
+            driverPool.get().quit();
+
+            driverPool.remove();
         }
     }
 }
